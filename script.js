0b030ee5d6acd7cbf6c7f2876d111e8a6bbd00ed
Samruddhi_Budde <buddesamruddhi@gmail.com>
2026-01-02 11:24:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+const apikey = "d8025a9581600b516bba45f7913286a5";
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkweather(city) {
+
+    if (city.trim() === "") return;
+
+    const response = await fetch(
+        `${apiurl}${encodeURIComponent(city.trim())}&appid=${apikey}`
+    );
+
+    if (!response.ok) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+        return;
+    }
+
+    const data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if (data.weather[0].main === "Clouds") {
+        weatherIcon.src = "img/clouds.png";
+    } else if (data.weather[0].main === "Clear") {
+        weatherIcon.src = "img/clear.png";
+    } else if (data.weather[0].main === "Rain") {
+        weatherIcon.src = "img/rain.png";
+    } else if (data.weather[0].main === "Drizzle") {
+        weatherIcon.src = "img/drizzle.png";
+    } else if (data.weather[0].main === "Mist") {
+        weatherIcon.src = "img/mist.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+}
+
+searchBtn.addEventListener("click", () => {
+    checkweather(searchBox.value);
+});
+
+
+searchBox.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+        checkweather(searchBox.value);
+    }
+});
+// checkweather("");
